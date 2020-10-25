c109c5616b13b0d4c5f7d5c3e8f08ff2962cbc2e
Szymon Wróbel <szym.wrobel@gmail.com>
2020-10-25 19:31:21 +0100

    Added function saving information to .json file. Sheeps array now stores all the sheeps (we do not discriminate the dead ones). Added function returning number of the alive sheeps. Changed program in few places due to these changes.

--- pythonProject/Wolf.py
+++ pythonProject/Wolf.py
@@ -8,32 +8,28 @@ class Wolf (Animal):
         super().__init__(move_range)
         self.position = [0.0, 0.0]
 
-    #COMMIT_COMMENT Function's name changed
     def move(self, sheep):
         distance = self.distance_to(sheep)
         x0, y0 = self.position
         x1, y1 = sheep.position
 
-        #COMMIT_COMMENT Bug in the formula fixed
         self.position = [x0 + self.move_range * (x1 - x0) / distance,
                          y0 + self.move_range * (y1 - y0) / distance]
-        #COMMIT_COMMENT Deleted "debug print" as it already works
 
-    #COMMIT_COMMENT Function added as it was neccessary
     def eat_sheep(self, unlucky_sheep, sheeps):
         self.position = unlucky_sheep.position
         unlucky_sheep.sheep_killed()
-        sheeps.remove(unlucky_sheep)
 
 
     def find_closest_sheep(self, sheeps) -> Sheep:
-        #COMMIT_COMMENT Variable name distance changed to lowest_distance
-        lowest_distance = self.distance_to(sheeps[0])
-        closest_sheep = sheeps[0]
         for sheep in sheeps:
-            # COMMIT_COMMENT <= sign turned into < as it changes nothing bad and
-            # doesnt override variables if the distances are equal
-            if self.distance_to(sheep) < lowest_distance:
+            if sheep.alive:
+                lowest_distance = self.distance_to(sheep)
+                closest_sheep = sheep
+                break
+
+        for sheep in sheeps:
+            if sheep.alive and self.distance_to(sheep) < lowest_distance:
                 lowest_distance = self.distance_to(sheep)
                 closest_sheep = sheep
         return closest_sheep
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,11 +1,30 @@
 from Sheep import Sheep
 from Wolf import Wolf
+import json
 
 
 sheep_move_dist = 0.5
 wolf_move_dist = 2.0
 turn_limit = 50
 
+def write_to_json(round_number, wolf, sheeps):
+    sheeps_info = []
+    for sheep in sheeps:
+        if sheep.alive == True:
+            sheeps_info.append(f"Sheep no {sheep.id}: {sheep.position[0]}, {sheep.position[1]}")
+        else:
+            sheeps_info.append(f"Sheep no {sheep.id}: null")
+    content = {
+        "round_no": round_number,
+        "wolf_pos": f"{wolf.position[0]}, {wolf.position[1]}",
+        "sheeps_info": sheeps_info
+    }
+    if round_number == 1:
+        file = open("pos.json", "w")
+    else:
+        file = open("pos.json", "a")
+    file.write(json.dumps(content, indent = 2))
+    file.close()
 
 def setup(sheep_no, init_pos_lim):
     sheep_list = [Sheep(init_pos_lim, sheep_move_dist, x)
@@ -13,34 +32,35 @@ def setup(sheep_no, init_pos_lim):
     wolf = Wolf(wolf_move_dist)
     return sheep_list, wolf
 
+def get_alive_sheeps(sheeps):
+    i = 0
+    for sheep in sheeps:
+        if sheep.alive == True:
+            i = i + 1
+    return i
 
 def simulate(wolf, sheeps):
     for turn in range(turn_limit):
-        #COMMIT_COMMENT If added to end the simulation if there is no sheep left
-        # Also handling index out of range error in Wolf.distance_to function if
-        # there is no sheep with index 0
-        if (len(sheeps) == 0):
+        if (get_alive_sheeps(sheeps) == 0):
             return
         for sheep in sheeps:
-            sheep.move()
+            if sheep.alive == True:
+                sheep.move()
         closest_sheep = wolf.find_closest_sheep(sheeps)
         if wolf.distance_to(closest_sheep) <= wolf.move_range:
-            #COMMIT_COMMENT Changed this if
             wolf.eat_sheep(closest_sheep, sheeps)
         else:
             wolf.move(closest_sheep)
 
         log(turn + 1, wolf, sheeps, closest_sheep)
+        write_to_json(turn + 1, wolf, sheeps)
 
-
-#COMMIT_COMMENT Changed wolf's position display, information about the eaten
-# sheep and formatted output
 def log(turn_count, wolf, sheeps, closest_sheep):
     print(f"Turn no: {turn_count}")
     print(f"Wolf position: ({round(wolf.position[0], 3)}, {round(wolf.position[1], 3)})")
     if (closest_sheep.alive == False):
         print(f"The sheep with number {closest_sheep.id} was eaten!")
-    print(f"Sheeps alive: {len(sheeps)}")
+    print(f"Sheeps alive: {get_alive_sheeps(sheeps)}")
     print("------------------------------\n")
 
 
