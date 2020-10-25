db81ff4ff957fca11cd6fe519e7d3cb15073420c
Szymon Wróbel <szym.wrobel@gmail.com>
2020-10-25 19:24:19 +0100

    Commit comments deleted

--- pythonProject/Sheep.py
+++ pythonProject/Sheep.py
@@ -3,10 +3,8 @@ from Animal import Animal
 
 
 class Sheep (Animal):
-    #COMMIT_COMMENT Added id and life status
     def __init__(self, init_pos_lim, move_range, id):
         super().__init__(move_range)
-        # COMMIT_COMMENT Strange implementation of rand.random changed to rand.uniform
         self.position = [rand.uniform(-init_pos_lim, init_pos_lim),
                          rand.uniform(-init_pos_lim, init_pos_lim)]
         self.id = id
@@ -14,7 +12,6 @@ class Sheep (Animal):
 
     def move(self):
         direction = rand.randint(1, 4)
-        # COMMIT_COMMENT Bug fix - Incorrect if deleted.
         if direction == 1:
             self.position[0] += self.move_range
         elif direction == 2:
@@ -24,6 +21,5 @@ class Sheep (Animal):
         elif direction == 4:
             self.position[1] -= self.move_range
 
-    #COMMIT_COMMENT Added function which changes alive status
     def sheep_killed(self):
         self.alive = False
